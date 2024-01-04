a3a01c22ac4fdf1fe2e871e88efe6fe6e318a7f4
Osmanbran <osmanbran@gmail.com>
2024-01-04 21:38:49 +1100

    changes

--- src/model.py
+++ src/model.py
@@ -13,29 +13,44 @@ class Model:
         self.ewma_slow = 0
         self.ewma_fast = 0
 
-    def evaluate(self):
+    def evaluate(self):    
+        # If ewma is the same, skip this evaluation, order has already been bought or sold.
+        if self.ewma_slow == self.ewma_fast:
+            return
+
+        prev_state = self.ewma_slow > self.ewma_fast
         new_price = self.market_data.poll()
         self.eval_ewma_fast(new_price)
         self.eval_ewma_slow(new_price)
+        new_state = self.ewma_slow >= self.ewma_fast
 
-        if fast_exceeds_slow():
+        if Model.fast_exceeds_slow(prev_state, new_state):
             return Result.BUY
         
-        if fast_falls_below_slow():
+        if Model.slow_exceeds_fast(prev_state, new_state):
             return Result.SELL
         
         else:
             return Result.NOTHING
 
+    def fast_exceeds_slow(prev_state, new_state):
+        # State true if slow higher than fast.
+        # Fast exceeded if slow was higher but is now lower.
+        return prev_state and not new_state
+
+    def slow_exceeds_fast(prev_state, new_state):
+        # State true if slow higher than fast.
+        # Slow exceeds if slow was lower but is now higher.
+        return not prev_state and new_state
 
     def eval_ewma_slow(self, new_price):
         weighted_new_price = new_price / self.n_fast
-        weighted_last_price = self.ewma_slow_arr[self.i_slow] / self.n_slow
+        weighted_last_price = self.ewma_slow_arr[self.i_slow]
         self.ewma_slow = self.ewma_slow - weighted_last_price + weighted_new_price
+        self.ewma_slow_arr[self.i_slow] = weighted_new_price
 
     def eval_ewma_fast(self, new_price):
         weighted_new_price = new_price / self.n_fast
-        weighted_last_price = self.ewma_fast_arr[self.i_fast] / self.n_fast
+        weighted_last_price = self.ewma_fast_arr[self.i_fast]
         self.ewma_fast = self.ewma_fast - weighted_last_price + weighted_new_price
-    
-    def fast_exceeds_slow()
+        self.ewma_fast_arr[self.i_fast] = weighted_new_price
