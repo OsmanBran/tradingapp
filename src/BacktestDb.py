c418d7692b2a1ad1339112f1d74323798934c042
Osmanbran <osmanbran@gmail.com>
2024-01-06 15:32:47 +1100

    update

---
+++ src/BacktestDb.py
@@ -0,0 +1,54 @@
+import time
+import requests
+from pymongo import MongoClient
+
+class BacktestDb:
+    def __init__(self):
+        connection_string = "mongodb://localhost:27017/"
+        self.client = MongoClient(connection_string)
+        self.db = self.client["local"]
+        self.collection = None
+        self.interval_seconds = 30
+        self.counter = 0
+
+        self.url = "https://api.btcmarkets.net/v3/markets/BTC-AUD/ticker"
+
+    
+    def start_session(self, collection_str: str, interval_seconds: int):
+        self.collection = self.db[collection_str]
+        self.interval_seconds = interval_seconds
+        self.counter = 0
+        
+        while True:
+            response = requests.get(self.url)
+            
+            if response.status_code == 200:
+                data = response.json()
+                data["_id"] = self.counter
+                self.counter += 1
+                self.push_update(data)
+                print(data)            
+            else:
+                print(f"Error: {response.status_code}")
+
+            time.sleep(self.interval_seconds)
+    
+    def push_update(self, data):
+        self.collection.insert_one(data)
+
+test_str = {
+"marketId": "BAT-AUD",
+"bestBid": "0.2612",
+"bestAsk": "0.2677",
+"lastPrice": "0.2652",
+"volume24h": "6392.34930418",
+"volumeQte24h": "1.39",
+"price24h": "130",
+"pricePct24h": "0.002",
+"low24h": "0.2621",
+"high24h": "0.2708",
+"timestamp": "2019-09-01T10:35:04.940000Z"
+}
+
+testDb = BacktestDb()
+testDb.start_session("test_two", 30)
